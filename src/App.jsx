aa897f7a70a13195fcb15898b4000cfbf0f8ef3a
nabilk11 <khan.nabiln11@gmail.com>
2022-02-15 23:06:25 -0500

    added context file for theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
+import { useContext } from 'react';
 import './App.css';
+import { ThemeContext } from './components/contexts/theme';
 import { Registration } from './components/Registration/Registration';
 
 function App() {
+  // setting up theme
+  const theme = useContext(ThemeContext)
+  console.log("theme", theme)
   return (
     <main className="App">
       <Registration />
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -13,7 +13,7 @@ export const Registration = () => {
 // user state
     const [user, setUser] = useState('');
     const [validName, setValidName] = useState(false)
-    const [userFocus, setUserFocuse] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
 // password state
     const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false)
@@ -26,15 +26,60 @@ export const Registration = () => {
     const [errMsg, setErrMsg] = useState('')
     const [success, setSuccess] = useState(false)
 
-    // useEffect to set focus when component loads
+// useEffect to set focus when component loads
     useEffect(() => {
         userRef.current.focus()
     }, [])
 
+// useEffect to check userName against regex - setting valid name to true or false
+    useEffect(() => {
+        const result = USERNAME_EXP.test(user)
+        console.log(result)
+        console.log(user)
+        setValidName(result)
+    }, [user])
+
+// useEffect to check password against regex - then validate with matching password
+    useEffect(() => {
+        const result = PWD_EXP.test(pwd)
+        console.log(result)
+        console.log(pwd)
+        setValidPwd(result)
+        const match = pwd === matchPwd
+        setValidMatch(match)
+    }, [pwd, matchPwd])
+
+// useEffect for the error message
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd, matchPwd])
+
   return (
-    <div>Registration
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} 
+        aria-live="assertive">{errMsg}</p>
+        <h1 className="title">Register</h1>
+    <form action="">
+        <label htmlFor="username"></label>
+        <input type="text" 
+        id='username' 
+        ref={userRef} 
+        autoComplete='off' 
+        onChange={(e) => setUser(e.target.value)} 
+        required
+        aria-invalid={validName ? "false" : "true"}
+        aria-describedby="uidnote"
+        onFocus={() => setUserFocus(true)}
+        onBlur={() => setUserFocus(false)}
+        />
+        <p id='uidnote' className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+            <FontAwesomeIcon icon={faInfoCircle} />
 
+        </p>
+    
+    <label htmlFor=""></label><label htmlFor=""></label>
+</form>
 
-    </div>
+    </section>
   )
 }
